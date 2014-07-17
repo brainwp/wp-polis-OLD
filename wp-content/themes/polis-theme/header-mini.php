24a163505cec191a796267b77a7534f0bbd74e6a
matheus <contato@matheusgimenez.com.br>
2014-07-17 18:25:05 -0300

    archives

--- wp-content/themes/polis-theme/header-mini.php
+++ wp-content/themes/polis-theme/header-mini.php
@@ -27,5 +27,8 @@
         </a>
     </header>
     <nav class="col-md-12 nav" id="nav">
+        <a href="<?php bloginfo('url');?>">
+            <img src="<?php bloginfo('template_url');?>/img/logo-mini.png">
+        </a>
         <?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
     </nav>
