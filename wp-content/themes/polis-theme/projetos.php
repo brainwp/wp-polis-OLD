453e0b6c15920c57302de54e47b122dae9dc5014
everaldomatias <everaldo@brasa.art.br>
2014-07-29 16:58:32 -0300

    Adicionando campo de equipes na single Projetos

--- wp-content/themes/polis-theme/projetos.php
+++ wp-content/themes/polis-theme/projetos.php
@@ -41,12 +41,6 @@ get_header();?>
                     </ul>
                 </div>
 
-                <aside class="col-md-4 pull-right sidebar-page">
-                    <?php if ( is_active_sidebar( 'widgets-institucional' ) ) : ?>
-                        <?php dynamic_sidebar( 'widgets-institucional' ); ?>
-                    <?php endif; ?>
-                </aside>
-
                 <section class="col-md-12 tabDetails atividades">
                     <div class="tabContents aba-area">
                         <?php // aqui vai o loop ?>
--- wp-content/themes/polis-theme/single-projetos.php
+++ wp-content/themes/polis-theme/single-projetos.php
@@ -31,6 +31,34 @@ get_header(); ?>
             <?php the_content(); ?>
         </article>
         <aside class="col-md-4 pull-right sidebar-page">
+
+             <div class="col-lg-12 clear"></div>
+            <?php if (get_field('projetos_repeater')): ?>
+            <aside class="equipe">
+                <div class="top">
+                    <a href="<?php the_permalink(); ?>#equipe_todos" class="detalhes">Equipe</a>
+                </div>
+                <ul>
+                        <?php $_repeater_i = 1; ?>
+                        <?php while (has_sub_field('projetos_repeater')): ?>
+                            <?php
+                            if($_repeater_i <= 4){
+                                $user = get_user_by( 'email', get_sub_field('projetos_email') );
+                                if($user){
+                                    $href = get_bloginfo('url') . '/equipe/' . $user->user_login;
+                                    echo '<li><a href="' . $href . '"> ' . get_sub_field('projetos_nome') . ' </a></li>';
+                                }
+                                else{
+                                    echo '<li><a> ' . get_sub_field('projetos_nome') . ' </a></li>';
+                                }
+                                $_repeater_i++;
+                            }
+                            ?>
+                        <?php endwhile; ?>
+                </ul>
+            </aside>
+            <?php endif; ?>
+
             <?php if ( is_active_sidebar( 'widgets-institucional' ) ) : ?>
                 <?php dynamic_sidebar( 'widgets-institucional' ); ?>
             <?php endif; ?>
