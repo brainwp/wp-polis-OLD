b84024b9653250ac21c5b7d2c01cbdbe31307200
everaldomatias <matiaseveraldo@gmail.com>
2014-08-18 17:27:11 -0300

    Alterando procedimento de migracao

--- wp-content/themes/polis-theme/page-publicacoes-migrate.php
+++ wp-content/themes/polis-theme/page-publicacoes-migrate.php
@@ -10,23 +10,27 @@ global $wpdb;
 $page = $_GET['pagina'];
 $args = array (
     'post_type'              => array('publicacoes'),
+    'post_status'            => array('draft'),
     'posts_per_page'         => '140',
-    'paged' => $page
+    'paged'         => $page,
 );
-
+ 
 // The Query
 $query = new WP_Query( $args );
-
+ 
 // The Loop
 if ( $query->have_posts() ) {
     while ( $query->have_posts() ) {
         $query->the_post();
         $post_id = get_the_ID();
-        $field_content = get_the_content_publicacoes();
-        $field_exp = explode('.,',$field_content);
-
-        update_post_meta($post_id, 'publicacoes_content', $field_exp[0]);
-        update_post_meta($post_id, 'publicacoes_content_bk', $field_content);
+        $field_content = get_the_content();
+        $content = wp_strip_all_tags($field_content);
+        $content .= ','.escape_terms('tag', 'name');
+        $content .= ','.escape_terms('autor', 'name');
+        $content .= ','.escape_terms('organizador', 'name');
+ 
+        $wpdb->update($wpdb->posts, array('post_content' => $content), array('id' => $post_id));
+        update_post_meta($post_id, 'publicacoes_content', $field_content);
     }
     echo 'sucesso?';
 }
